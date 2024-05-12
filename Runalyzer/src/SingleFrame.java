f083505a5729c26be6215fe8f33f7c0b2431402b
Benjamin Heuser <Benjamin@Karstens-MBP.fritz.box>
2024-05-12 16:56:40 +0200

    changed moments line 56 after analysis changed location of ObjectDetection

--- Runalyzer/src/SingleFrame.java
+++ Runalyzer/src/SingleFrame.java
@@ -46,26 +46,26 @@ public class SingleFrame {
         BackgroundSubtraction backgroundSubtractor = new BackgroundSubtraction();
         Mat differenceImg = backgroundSubtractor.subtract(backgroundFrame, frame);
 
+        //filter the image to remove noise
+        ObjectDetection objDetector = new ObjectDetection();
+        differenceImg = objDetector.detectObject(differenceImg);
+
         Moments moments = Imgproc.moments(differenceImg);
         //get_m00 counts number of white pixels in the image, if enough pixels counted there exists a runner...
         //TODO: check what's a correct value to identify a runner (depends also on camera-distance)
-        if(moments.get_m00() > 50){
+        if(moments.get_m00() > 900000){
             hasRunner = true;
             runnerPosition.setX((int) (moments.get_m10() / moments.get_m00()));
             runnerPosition.setY((int) (moments.get_m01() / moments.get_m00()));
-            ObjectDetection objDetector = new ObjectDetection();
-            objDetector.detectObject(differenceImg); // new line of code, might not work...
             runnerWidth = objDetector.getObjectWidth();
             runnerHeight = objDetector.getObjectHeight();
-        }
+        } 
         else{
             hasRunner = false;
         }
     }
 
-    public int getRunnerWidth(){
-        return runnerWidth;
-    }
+    public int getRunnerWidth(){ return runnerWidth; }
 
     public int getRunnerHeight(){
         return runnerHeight;
