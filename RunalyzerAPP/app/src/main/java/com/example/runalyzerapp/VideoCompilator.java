e02f917306506994aacbcf5db5f67d235e4c5260
Max Hentschel <max.hentschel@e-t-a.de>
2024-07-22 11:01:02 +0200

    added error handling

--- RunalyzerAPP/app/src/main/java/com/example/runalyzerapp/VideoCompilator.java
+++ RunalyzerAPP/app/src/main/java/com/example/runalyzerapp/VideoCompilator.java
@@ -7,6 +7,7 @@ import org.opencv.imgcodecs.Imgcodecs;
 
 import java.util.ArrayList;
 import java.util.List;
+import java.util.Objects;
 
 public class VideoCompilator {
     private List<Mat> finalFrames = new ArrayList<>();
@@ -18,13 +19,25 @@ public class VideoCompilator {
         this.videoHeight = videoHeight;
     }
 
-    public void selectFinalFrames(List<VideoSequence> videoSequences){
+    public String selectFinalFrames(List<VideoSequence> videoSequences){
         Log.d("Benni", "Selecting Final Frames");
+        if(videoSequences.isEmpty()){
+            Log.d("Benni", "VideoCompilator: selectFinalFrames(): No video sequences");
+            return ("No video sequences to select final frames.");
+        }
         double relativeRunnerPosition;
         double switchingTimecode = 0;
         for(VideoSequence vs : videoSequences){
+            if(vs.getSingleFrames().isEmpty()){
+                Log.d("Benni", "VideoCompilator: selectFinalFrames(): No single frames in video sequence");
+                return ("No single frames in video sequence, final frames can't be selected.");
+            }
             for(SingleFrame fr : vs.getSingleFrames()){
                 if(fr.hasRunner() && fr.getTimecode() >= switchingTimecode){
+                    if(fr.getRunnerPosition().getX() == 0 || fr.getFrame().width() == 0){
+                        Log.d("Benni", "VideoCompilator: selectFinalFrames(): Runner position or frame width is 0");
+                        return ("Runner position or frame width is 0, final frame can't be selected.");
+                    }
                     relativeRunnerPosition = (double) fr.getRunnerPosition().getX() / fr.getFrame().width();
                     if(vs != videoSequences.get(videoSequences.size() - 1) && relativeRunnerPosition >= 0.875){
                         switchingTimecode = fr.getTimecode();
@@ -33,7 +46,6 @@ public class VideoCompilator {
                     else{
                         if(fr.getCroppedFrame() != null){
                             finalFrames.add(fr.getCroppedFrame());
-                            Log.d("Benni", "Cropped Frame added to finalFrames");
                         }
                     }
                 }
@@ -42,14 +54,21 @@ public class VideoCompilator {
                 //Log.d("Benni", "Frame added to finalFrames");
             }
         }
+        return "success";
     }
 
-    public void createFinalVideo(){
+    public String createFinalVideo(){
+        String retval = null;
         VideoFrameProcessor processorFinalVideo = new VideoFrameProcessor();
         if(!finalFrames.isEmpty()){
-            processorFinalVideo.framesToVideo(finalFrames);
+            retval = processorFinalVideo.framesToVideo(finalFrames);
+            if(!Objects.equals(retval, "success")){
+                return retval;
+            }
         }else{
-            //TODO: error handling
+            Log.d("Benni", "VideoCompilator: createFinalVideo(): No final frames");
+            return ("No final frames available, final video can't be created.");
         }
+        return "success";
     }
 }
