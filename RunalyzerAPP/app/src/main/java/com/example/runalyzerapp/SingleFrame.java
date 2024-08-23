c3bed60ef60c0a4a63291ffe07e9eb7f3dfe6127
Max Hentschel <max.hentschel@e-t-a.de>
2024-08-23 13:21:03 +0200

    added logic to also use frames where runner is at the edge

--- RunalyzerAPP/app/src/main/java/com/example/runalyzerapp/SingleFrame.java
+++ RunalyzerAPP/app/src/main/java/com/example/runalyzerapp/SingleFrame.java
@@ -1,20 +1,11 @@
 package com.example.runalyzerapp;
 
-import android.content.ContentValues;
-import android.content.Context;
-import android.graphics.Bitmap;
-import android.net.Uri;
-import android.provider.MediaStore;
+import static org.opencv.core.CvType.CV_32F;
 import android.util.Log;
-
-import org.opencv.android.Utils;
 import org.opencv.core.Mat;
 import org.opencv.core.Rect;
+import org.opencv.core.Size;
 import org.opencv.imgproc.Imgproc;
-import org.opencv.imgproc.Moments;
-
-import java.io.IOException;
-import java.io.OutputStream;
 
 public class SingleFrame {
     private Mat frame;
@@ -68,29 +59,56 @@ public class SingleFrame {
         return timecode;
     }
 
-    public String cropFrame(int width, int height){
-        if(width == 0 || height == 0){
-            Log.d("Benni", "SingleFrame: cropFrame(): Width or Height is 0");
-            return ("Width or Height is 0, frame can't be cropped.");
+    public String cropFrame(int width, int height, int yPosition){
+        if(width == 0 || height == 0 || yPosition == 0){
+            Log.d("Benni", "SingleFrame: cropFrame(): Width, Height or Y-Position is 0");
+            return ("Width, Height or Y-Position is 0, frame can't be cropped.");
         }
         if(hasRunner){
-            if(runnerInformation.getRunnerPosition().getX() == 0 || runnerInformation.getRunnerPosition().getY() == 0){
+            if(runnerInformation.getRunnerPosition().getX() == 0 && runnerInformation.getRunnerPosition().getY() == 0){
                 Log.d("Benni", "SingleFrame: cropFrame(): No runner position available");
                 return ("No runner position available, frame can't be cropped.");
             }
-            int xStartPos = runnerInformation.getRunnerPosition().getX()-(width/2);
-            int yStartPos = runnerInformation.getRunnerPosition().getY()-(height/2);
-
-            //TODO: find solution to crop Frame when Runner is at the edge
             if(frame.width() == 0|| frame.height() == 0){
                 Log.d("Benni", "SingleFrame: cropFrame(): Frame size is 0");
                 return ("No frame size available, frame can't be cropped.");
             }
-            if(xStartPos > 0 && xStartPos < (frame.width()-width)){
-                if(yStartPos > 0 && yStartPos < (frame.height()-height)){
-                    Rect rectCrop = new Rect(xStartPos, yStartPos, width, height);
-                    croppedFrame = new Mat(frame,rectCrop);
-                }
+
+            int xStartPos = runnerInformation.getRunnerPosition().getX() - (width/2);
+            int yStartPos = yPosition - (height/2);
+            int xShift = 0;
+            int yShift = 0;
+            boolean imgShiftingNeeded = false;
+
+            if(xStartPos < 0){
+                imgShiftingNeeded = true;
+                xShift = -xStartPos;
+                xStartPos = 0;
+            }else if(xStartPos > (frame.width()-width)){
+                imgShiftingNeeded = true;
+                xShift = (frame.width()-width) - xStartPos;
+                xStartPos = frame.width()-width;
+            }
+
+            if(yStartPos < 0){
+                imgShiftingNeeded = true;
+                yShift = -yStartPos;
+                yStartPos = 0;
+            }else if(yStartPos > (frame.height()-height)){
+                imgShiftingNeeded = true;
+                yShift = (frame.height()-height) - yStartPos;
+                yStartPos = frame.height()-height;
+            }
+
+            Rect rectCrop = new Rect(xStartPos, yStartPos, width, height);
+            croppedFrame = new Mat(frame,rectCrop);
+
+            if(imgShiftingNeeded){
+                Mat transformationMat = new Mat(2,3, CV_32F);
+                transformationMat.put(0,0, 1); transformationMat.put(0,1, 0); transformationMat.put(0,2, xShift);
+                transformationMat.put(1,0, 0); transformationMat.put(1,1, 1); transformationMat.put(1,2, yShift);
+                Imgproc.warpAffine(croppedFrame, croppedFrame, transformationMat, new Size(width, height));
+                transformationMat.release();
             }
         }
         return "success";
