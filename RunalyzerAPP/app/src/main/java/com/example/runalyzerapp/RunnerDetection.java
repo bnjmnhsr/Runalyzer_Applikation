062641282bc122b455abee2d42918dafe9cfea6d
Max Hentschel <max.hentschel@e-t-a.de>
2024-11-02 12:07:33 +0100

    RunnerDetection cleaned up

--- RunalyzerAPP/app/src/main/java/com/example/runalyzerapp/RunnerDetection.java
+++ RunalyzerAPP/app/src/main/java/com/example/runalyzerapp/RunnerDetection.java
@@ -1,7 +1,5 @@
 package com.example.runalyzerapp;
 
-import android.content.Context;
-
 public interface RunnerDetection {
-    public RunnerInformation detectRunnerInformation(SingleFrame singleFrame, Context context);
+    public RunnerInformation detectRunnerInformation(SingleFrame singleFrame);
 }
